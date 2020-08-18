1d7ee3781819be3298535a58a827376b7bf2069f
Juan José Pérez <juanjoseperez@MacBook-Pro-de-Juan.local>
2020-08-18 17:38:15 +0200

    few changes

--- src/membersList.tsx
+++ src/membersList.tsx
@@ -1,47 +1,50 @@
 import React, { useEffect } from "react";
 import { Link, generatePath } from "react-router-dom";
 import { useDebounce } from "use-debounce";
-import {ImageAvatars} from "./memberAvatar";
-import {MemberEntity} from "./model"
-import {withStyles, makeStyles} from '@material-ui/core/styles'
-import Table from "@material-ui/core/Table"
-import TableBody from "@material-ui/core/TableBody"
-import TableCell from "@material-ui/core/TableCell"
-import TableContainer from "@material-ui/core/TableContainer"
-import TableHead from "@material-ui/core/TableHead"
-import TableRow from "@material-ui/core/TableRow"
-import Paper from "@material-ui/core/Paper"
+import { ImageAvatars } from "./memberAvatar";
+import { MemberEntity } from "./model";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 import { TablePagination } from "@material-ui/core";
 
 const StyledTabelCell = withStyles((theme) => ({
   head: {
     backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white
+    color: theme.palette.common.white,
   },
   body: {
     fontSize: 14,
-  }
+  },
 }))(TableCell);
 
 const StyledTableRow = withStyles((theme) => ({
   root: {
-    '&:nth-of-type(odd)': {
+    "&:nth-of-type(odd)": {
       backgroundColor: theme.palette.action.hover,
+    },
+    "&:hover": {
+      backgroundColor: theme.palette.action.focus,
     }
-  }
+  },
 }))(TableRow);
 
 const useStyles = makeStyles({
   table: {
     minWidth: 700,
-  }
-})
+  },
+});
 
 export const ListPage: React.FC = () => {
   const [organizationFilter, setorganizationFilter] = React.useState(
     "lemoncode",
   );
-  const [debounceFilter] = useDebounce(organizationFilter, 800);
+  const [debounceFilter] = useDebounce(organizationFilter, 1000);
   const [members, setMembers] = React.useState<MemberEntity[]>([]);
   const classes = useStyles();
 
@@ -63,7 +66,6 @@ export const ListPage: React.FC = () => {
         <Table className={classes.table} aria-label="customized table">
           <TableHead>
             <TableRow>
-              <StyledTabelCell>Id</StyledTabelCell>
               <StyledTabelCell>Login</StyledTabelCell>
               <StyledTabelCell>Avatar</StyledTabelCell>
             </TableRow>
@@ -71,11 +73,17 @@ export const ListPage: React.FC = () => {
           <TableBody>
             {members.map((member) => (
               <StyledTableRow key={member.id}>
-                <StyledTabelCell component="th" scope="member">
-                  {member.id}
+                <StyledTabelCell>
+                  <ImageAvatars
+                    login={member.login}
+                    avatar_url={member.avatar_url}
+                  ></ImageAvatars>
                 </StyledTabelCell>
-                <StyledTabelCell>{member.login}</StyledTabelCell>
-                <StyledTabelCell><ImageAvatars login={member.login} avatar_url={member.avatar_url}></ImageAvatars></StyledTabelCell>
+                <StyledTabelCell component="th" scope="member">
+                  <Link to={generatePath("/detail/:id", { id: member.login })}>
+                    {member.login}
+                  </Link>
+                </StyledTabelCell>
               </StyledTableRow>
             ))}
           </TableBody>
