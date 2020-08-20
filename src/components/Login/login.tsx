43be29d94c18c9c1b55da19295a04ff8f3c4efb5
Juan José Pérez <juanjoseperez@MacBook-Pro-de-Juan.local>
2020-08-20 16:57:46 +0200

    styling login

--- src/components/Login/login.tsx
+++ src/components/Login/login.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import { styled } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
 export const LoginPage: React.FC = () => {
   const history = useHistory();
@@ -16,35 +19,43 @@ export const LoginPage: React.FC = () => {
     }
   };
 
-  return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="100vh"
-    >
-      <form onSubmit={handleNavigation}>
-        <h2>Hello from login page</h2>
-        <div>
-          <div>
-            <label>Username: </label>
-            <input
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Password: </label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-        </div>
+  const StyledButton = styled(Button)({
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 40,
+    padding: "0 30px",
+  });
 
-        <button type="submit">login</button>
-      </form>
-    </Box>
+  return (
+    <form onSubmit={handleNavigation}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <h2>Hello, enter your identification</h2>
+        <div>
+          <label>Username: </label>
+          <TextField
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password: </label>
+          <TextField
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <StyledButton type="submit">login</StyledButton>
+      </Box>
+    </form>
   );
 };
