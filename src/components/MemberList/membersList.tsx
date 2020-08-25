0ea9a7bd9ed32f21161f740c2588238a40aa8295
Juan José Pérez <juanjoseperez@MacBook-Pro-de-Juan.local>
2020-08-25 10:10:32 +0200

    api info ok

--- src/components/MemberList/membersList.tsx
+++ src/components/MemberList/membersList.tsx
@@ -15,29 +15,38 @@ import {
   Box,
   FormHelperText,
 } from "@material-ui/core";
-
 import { StyledTabelCell, StyledTableRow } from "./membersList.styles";
 
 const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
   table: {
     minWidth: 700,
   },
-  main: {
-    display: "flex",
-    flexDirection: "column",
-    alignContent: "center"
-  }
+  apiInfo: {
+    color: "white",
+  },
+  inputText: {
+    width: "30rem",
+    height: "2rem",
+    borderRadius: 5,
+    color: "#c0c0c0",
+  },
 });
 
 export const ListPage: React.FC = () => {
   const [organizationFilter, setorganizationFilter] = React.useState(
     "lemoncode",
   );
-  const [debounceFilter] = useDebounce(organizationFilter, 1000);
+  const [debounceFilter] = useDebounce(organizationFilter, 800);
   const [members, setMembers] = React.useState<MemberEntity[]>([]);
   const classes = useStyles();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [showApiInfo, setShowApiInfo] = React.useState(false);
 
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
@@ -50,66 +59,89 @@ export const ListPage: React.FC = () => {
     setPage(0);
   };
 
-  const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, members.length - page * rowsPerPage);
+  const handleShowApiInfo = (handle: boolean) => {
+    setShowApiInfo(handle);
+  };
+
+  const apiInfo = (
+    <div>
+      <div className={classes.apiInfo}>
+        There's no members or company doesn't exist
+      </div>
+    </div>
+  );
+
+  const membersTable = (
+    <Box width="40%" mx="auto">
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTabelCell>Login</StyledTabelCell>
+              <StyledTabelCell>Avatar</StyledTabelCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {members
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((member, index) => (
+                <StyledTableRow key={member.id}>
+                  <StyledTabelCell>
+                    <Link
+                      to={generatePath("/detail/:id", { id: member.login })}
+                    >
+                      <ImageAvatars
+                        login={member.login}
+                        avatar_url={member.avatar_url}
+                      ></ImageAvatars>
+                    </Link>
+                  </StyledTabelCell>
+                  <StyledTabelCell component="th" scope="member">
+                    {member.login}
+                  </StyledTabelCell>
+                </StyledTableRow>
+              ))}
+          </TableBody>
+          <TablePagination
+            rowsPerPageOptions={[5, 10]}
+            component="div"
+            count={members.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}
+          />
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+
+  const tableOrInfo = () => (showApiInfo ? apiInfo : membersTable);
 
   React.useEffect(() => {
     fetch(`https://api.github.com/orgs/${organizationFilter}/members`)
       .then((response) => response.json())
-      .then((json) => setMembers(json))
+      .then((json) => {
+        if (json.length > 0) {
+          setMembers(json);
+          handleShowApiInfo(false);
+        } else {
+          handleShowApiInfo(true);
+        }
+      })
       .catch((err) => console.error(err));
   }, [debounceFilter]);
 
   return (
-    <div className={classes.main}>
+    <div className={classes.root}>
       <Box mx="auto" marginBottom="20px">
         <input
+          className={classes.inputText}
           value={organizationFilter}
           onChange={(e) => setorganizationFilter(e.target.value)}
         />
       </Box>
-      <Box width="40%" mx="auto">
-        <TableContainer component={Paper}>
-          <Table className={classes.table} aria-label="customized table">
-            <TableHead>
-              <TableRow>
-                <StyledTabelCell>Login</StyledTabelCell>
-                <StyledTabelCell>Avatar</StyledTabelCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {members
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((member, index) => (
-                  <StyledTableRow key={member.id}>
-                    <StyledTabelCell>
-                      <Link
-                        to={generatePath("/detail/:id", { id: member.login })}
-                      >
-                        <ImageAvatars
-                          login={member.login}
-                          avatar_url={member.avatar_url}
-                        ></ImageAvatars>
-                      </Link>
-                    </StyledTabelCell>
-                    <StyledTabelCell component="th" scope="member">
-                      {member.login}
-                    </StyledTabelCell>
-                  </StyledTableRow>
-                ))}
-            </TableBody>
-            <TablePagination
-              rowsPerPageOptions={[5, 10]}
-              component="div"
-              count={members.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onChangePage={handleChangePage}
-              onChangeRowsPerPage={handleChangeRowsPerPage}
-            />
-          </Table>
-        </TableContainer>
-      </Box>
+      {tableOrInfo()}
     </div>
   );
 };
